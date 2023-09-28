e8afe3213c7559717b4d259449e16da323aac4e4
eunbi9n <gombi9_9@naver.com>
2023-09-29 03:46:01 +0900

    feat: 일반적인 message 형식으로 parsing

---
+++ srcs/Message.cpp
@@ -0,0 +1,74 @@
+#include "Message.hpp"
+
+Message::Message()
+{
+}
+
+Message::Message(const string &message)
+{
+	parseMessage(message);
+}
+
+Message::~Message()
+{
+}
+
+string Message::getPrefix()
+{
+	return this->_prefix;
+}
+
+string Message::getCommand()
+{
+	return this->_command;
+}
+
+vector<string> Message::getParams()
+{
+	return this->_params;
+}
+
+void Message::setPrefix(string prefix)
+{
+	this->_prefix = prefix;
+}
+
+void Message::setCommand(string command)
+{
+	this->_command = command;
+}
+
+void Message::setParams(vector<string> params)
+{
+	this->_params = params;
+}
+
+void Message::parseMessage(const string &message)
+{
+	std::string prefix;
+    std::string command;
+    std::vector<std::string> params;
+
+    size_t pos = 0;
+    if (message[0] == ':') {
+        pos = message.find(' ');
+        prefix = message.substr(1, pos - 1);
+    }
+
+    size_t start = pos;
+    pos = message.find(' ', start);
+    command = message.substr(start, pos - start);
+
+    while (pos != std::string::npos) {
+        start = pos + 1;
+        if (message[start] == ':') {
+            params.push_back(message.substr(start + 1));
+            break;
+        }
+        pos = message.find(' ', start);
+        params.push_back(message.substr(start, pos - start));
+    }
+	setPrefix(prefix);
+	setCommand(command);
+	setParams(params);
+}
