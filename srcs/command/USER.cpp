c51d6f94473b7f75b1863cf2070ad843d7c3d424
정성은 <soc06241@naver.com>
2023-10-03 14:25:21 +0900

    feat: 오류 케이스 추가

--- srcs/command/USER.cpp
+++ srcs/command/USER.cpp
@@ -5,15 +5,24 @@
 
 void Command::USER(Message &message, User *user)
 {
+	string	serverPrefix = this->_server->getServerPrefix();
+	if (message.getParams().size() < 4)
+		return sendToClient(user->getFd(), generateReply(serverPrefix, ERR_NEEDMOREPARAMS(user->getNickname(), "USER")));
+	
 	string username = message.getParams()[0];
 	string hostname = message.getParams()[1];
 	string servername = message.getParams()[2];
 	string realname = message.getParams()[3];
+	
+	if (user->getIsRegistered())
+		return sendToClient(user->getFd(), generateReply(serverPrefix, ERR_ALREADYREGISTERED(user->getNickname())));
+	if (message.getParams().size() < 4)
+		return sendToClient(user->getFd(), generateReply(serverPrefix, ERR_NEEDMOREPARAMS(user->getNickname(), "USER")));
+	
 	user->setNickname(username);
 	user->setHost(hostname);
-	this->_server->setServerName(servername);
 	user->setRealName(realname);
-	string welcome = ":irc.easy 001 " + username + " :Welcome to the Internet Relay Network " + username + "!" + hostname + "@" + servername + "\r\n";
+	string welcome = ":" + serverPrefix + " 001 " + username + " :Welcome to the Internet Relay Network ~" + username + "!" + hostname + "@" + servername + "\r\n";
 	sendToClient(user->getFd(), welcome);
 	cout << "USER: " << username << " " << hostname << " " << servername << " " << realname << endl;
 }
