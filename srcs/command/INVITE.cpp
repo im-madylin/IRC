3b8346d9feb62ce92b7314199dcf7592832a4d28
eunbi9n <gombi9_9@naver.com>
2023-10-04 21:12:10 +0900

    fix: INVITE 명령어 수정

--- srcs/command/INVITE.cpp
+++ srcs/command/INVITE.cpp
@@ -6,27 +6,32 @@
 void Command::INVITE(Message &message, User *user)
 {
 	string serverPrefix = this->_server->getServerPrefix();
+	string userPrefix = user->getUserPrefix();
+	string clientName = user->getNickname();
+	string targetName = message.getParams()[0];
+	string channelName = message.getParams()[1];
 	// ERR_NEEDMOREPARAMS
 	if (message.getParamsSize() < 2)
-		return sendToClient(user->getFd(), generateReply(serverPrefix, ERR_NEEDMOREPARAMS(user->getNickname(), "INVITE")));
+		return sendToClient(user->getFd(), generateReply(serverPrefix, ERR_NEEDMOREPARAMS(clientName, "INVITE")));
 	
-	Channel *channel = this->_server->findChannel(message.getParams()[1]);
+	Channel *channel = this->_server->findChannel(channelName);
 	if (channel == NULL)
-		return sendToClient(user->getFd(), generateReply(serverPrefix, ERR_NOSUCHCHANNEL(user->getNickname(), message.getParams()[0])));
+		return sendToClient(user->getFd(), generateReply(serverPrefix, ERR_NOSUCHCHANNEL(clientName, channelName)));
 	
-	User *target = this->_server->findUser(message.getParams()[0]);
+	User *target = this->_server->findUser(targetName);
 	if (target == NULL)
-		return sendToClient(user->getFd(), generateReply(serverPrefix, ERR_NOSUCHNICK(user->getNickname(), message.getParams()[1])));
+		return sendToClient(user->getFd(), generateReply(serverPrefix, ERR_NOSUCHNICK(clientName, channelName)));
 
-	if (channel->isExistUser(user->getFd()))
-		return sendToClient(user->getFd(), generateReply(serverPrefix, ERR_USERONCHANNEL(user->getNickname(), message.getParams()[0], message.getParams()[1])));
+	if (channel->isExistUser(target->getFd()))
+		return sendToClient(user->getFd(), generateReply(serverPrefix, ERR_USERONCHANNEL(clientName, targetName, channelName)));
 	
 	// invite-only 채녈인 경우는 operator만 가능
 	if (channel->getMode() == "i" && channel->isOperator(user) == false)
-		return sendToClient(user->getFd(), generateReply(serverPrefix, ERR_CHANOPRIVSNEEDED(user->getNickname(), message.getParams()[0])));
+		return sendToClient(user->getFd(), generateReply(serverPrefix, ERR_CHANOPRIVSNEEDED(clientName, targetName)));
 	
 	channel->addInvite(target->getFd());
-	// broadcast
 
-	sendToClient(user->getFd(), generateReply(serverPrefix, RPL_INVITING(user->getNickname(), target->getNickname(), channel->getChannelName())));
+	// MEMO: 나머지 멤버한테도 알려야 하나?
+	sendToClient(user->getFd(), generateReply(serverPrefix, RPL_INVITING(clientName, targetName, channelName)));
+	sendToClient(target->getFd(), generateReply(userPrefix, " INVITE " + targetName + " :" + channelName));
 }
