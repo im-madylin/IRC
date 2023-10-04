339ae0a5087d658564f316d5edcc1d7503d736ed
정성은 <soc06241@naver.com>
2023-10-04 17:56:42 +0900

    fix: broadcast 함수 호출 부분 수정

--- srcs/command/PRIVMSG.cpp
+++ srcs/command/PRIVMSG.cpp
@@ -13,28 +13,20 @@ void Command::PRIVMSG(Message &message, User *user)
 
 	vector<string>	recipients = split(message.getParams()[0], ",");
 	for(vector<string>::const_iterator it = recipients.begin(); it != recipients.end(); it++) {
-		bool checkDuplicateRecipients = false;
-		for(vector<string>::const_iterator jt = recipients.begin(); jt != it; jt++)
-			if (*it == *jt) {
-				checkDuplicateRecipients = true;
-				break;
-			}
-		if (checkDuplicateRecipients) {
-			sendToClient(user->getFd(), generateReply(serverPrefix, ERR_TOOMANYTARGETS(user->getNickname(), *it)));
-			continue;
-		}
-		if (it[0] == "#") {
+		if ((*it)[0] == '#') {
 			Channel *channel = _server->findChannel(*it);
 			if (channel == NULL) {
 				sendToClient(user->getFd(), generateReply(serverPrefix, ERR_NOSUCHNICK(user->getNickname(), *it)));
 				continue;
 			}
-			//broadcast();
+			broadcast(user->getFd(), channel, generateReply(serverPrefix, RPL_AWAY(user->getNickname(), *it, message.getParams()[1])));
 		}
-		User *recipient = _server->findUser(*it);
-		if (recipient == NULL)
-			sendToClient(user->getFd(), generateReply(serverPrefix, ERR_NOSUCHNICK(user->getNickname(), *it)));
-		else
-			sendToClient(recipient->getFd(), generateReply(serverPrefix, RPL_AWAY(user->getNickname(), *it, message.getParams()[1])));
+		else {
+			User *recipient = _server->findUser(*it);
+			if (recipient == NULL)
+				sendToClient(user->getFd(), generateReply(serverPrefix, ERR_NOSUCHNICK(user->getNickname(), *it)));
+			else
+				sendToClient(recipient->getFd(), generateReply(serverPrefix, RPL_AWAY(user->getNickname(), *it, message.getParams()[1])));
+		}
 	}
 }
