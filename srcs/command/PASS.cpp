39c422655df89aa71cb915b99d9eefa12bd06c50
dlwndms0812 <dlwndms0812@naver.com>
2023-10-06 17:17:29 +0900

    fix: PASS에 파라미터가 안들어올때 오류 수정

--- srcs/command/PASS.cpp
+++ srcs/command/PASS.cpp
@@ -9,9 +9,12 @@ void Command::PASS(Message &message, User *user)
 	string serverPrefix = this->_server->getServerPrefix();
 
 	//파라미터가 없는 경우
-	if (message.getParamsSize() < 1)
-		return sendToClient(user->getFd(), generateReply(serverPrefix, ERR_NEEDMOREPARAMS(user->getNickname(), "PASS")));
+	if (message.getParamsSize() < 1 || message.getParams()[0] == "")
+		return user->appendMessage(generateReply(serverPrefix, ERR_NEEDMOREPARAMS(user->getNickname(), "PASS")));
 	
+	if (user->getAuth() == true)
+		return user->appendMessage(generateReply(serverPrefix, ERR_ALREADYREGISTERED(user->getNickname())));
+
 	//server password와 비교
 	string password = message.getParams()[0];
 
