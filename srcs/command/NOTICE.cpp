cbf4f1d21ee76cddece8174c3e3ca9c5c3bef03d
정성은 <soc06241@naver.com>
2023-10-06 18:46:28 +0900

    fix: reply message 수정

--- srcs/command/NOTICE.cpp
+++ srcs/command/NOTICE.cpp
@@ -6,6 +6,8 @@
 void Command::NOTICE(Message &message, User *user)
 {
 	string	serverPrefix = this->_server->getServerPrefix();
+	string	userPrefix = user->getUserPrefix();
+
 	if (message.getParamsSize() == 0)
 		return sendToClient(user->getFd(), generateReply(serverPrefix, ERR_NORECIPIENT(user->getNickname(), "NOTICE")));
 	if (message.getParamsSize() == 1 || message.getParams()[1].length() == 0)
@@ -13,18 +15,18 @@ void Command::NOTICE(Message &message, User *user)
 
 	vector<string>	recipients = split(message.getParams()[0], ",");
 	for(vector<string>::const_iterator it = recipients.begin(); it != recipients.end(); it++) {
-		if (it[0] == "#") {
+		if ((*it)[0] == '#') {
 			Channel *channel = _server->findChannel(*it);
 			if (channel == NULL)
 				continue;
-            broadcast(user->getFd(), channel, generateReply(serverPrefix, RPL_AWAY(user->getNickname(), *it, message.getParams()[1])));
+            broadcast(user->getFd(), channel, ":" + userPrefix + " NOTICE " + channel->getChannelName() + " :" + message.getParams()[1] + "\r\n");
         }
         else {
             User *recipient = _server->findUser(*it);
             if (recipient == NULL)
                 continue;
             else
-                sendToClient(recipient->getFd(), generateReply(serverPrefix, RPL_AWAY(user->getNickname(), *it, message.getParams()[1])));
+                sendToClient(recipient->getFd(),":" + serverPrefix + " NOTICE " + *it + " :" + message.getParams()[1]);
         }
     }
 }
