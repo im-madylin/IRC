2e5cc1ea933ebed66064fad618772a02c87b95e3
dlwndms0812 <dlwndms0812@naver.com>
2023-10-06 14:36:13 +0900

    fix: QUIT 메세지 오류 수정

--- srcs/command/QUIT.cpp
+++ srcs/command/QUIT.cpp
@@ -8,20 +8,18 @@ void Command::QUIT(Message &message, User *user)
 {
     string serverPrefix = this->_server->getServerPrefix();
 
-    string msg = ":" + serverPrefix + " 001 " + user->getNickname() + " :QUIT GoodBye!" + "\r\n";
+    string msg = ":" + serverPrefix + " 001 " + user->getNickname() + " :QUIT ";
 	
-	
-    if (message.getParamsSize() < 1)
-        sendToClient(user->getFd(), msg + "GoodBye");
+    if (message.getParamsSize() <= 1 && message.getParams()[0] == "")
+        sendToClient(user->getFd(), msg + "GoodBye " + "\r\n");
     else
     {
         //TODO: message의 params이 들어오는대로 msg를 출력해야하나? ex)QUIT go to lunch 인 경우 msg를 QUIT: go to lunch 로..?
 
         //일단 Param이 1개로 가정
-        sendToClient(user->getFd(), msg + message.getParams()[0]);
+        sendToClient(user->getFd(), msg + message.getParams()[0] + "\r\n");
     }
 
-    //TODO: msg 보낸 후, 클라이언트와 접속 끊기
-    //MEMO: 따로 처리안했는데, /QUIT 명령어를 하면 약 3~5초 뒤에 limechat에서 disconnect 처리됨
-    cout << "disconnect client" << endl;
+    //TODO: disconnect가 자동으로 되지만 되기까지 시간이 느림
+    
 }
