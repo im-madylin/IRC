283ac96131fe215f1bd6e61cbbd09861da4e3a58
dlwndms0812 <dlwndms0812@naver.com>
2023-10-06 16:56:49 +0900

    fix: PASS에 혹시모를 segfault 대비

--- srcs/command/PASS.cpp
+++ srcs/command/PASS.cpp
@@ -8,16 +8,13 @@ void Command::PASS(Message &message, User *user)
 {
 	string serverPrefix = this->_server->getServerPrefix();
 
-	string password = message.getParams()[0];
-
 	//파라미터가 없는 경우
-	if (password.size() == 0)
+	if (message.getParamsSize() < 1)
 		return sendToClient(user->getFd(), generateReply(serverPrefix, ERR_NEEDMOREPARAMS(user->getNickname(), "PASS")));
 	
-	//TODO: 이미 등록되어 있는 경우
-	
-	//TODO: 수정 필요
 	//server password와 비교
+	string password = message.getParams()[0];
+
 	if (password == this->_server->getPassword())
 		user->setAuth();
 	else
